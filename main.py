90334af771eba7a255393ce40c65a82e41b5a8ee
Сергей <vorona07@mail.ru>
2023-01-09 15:55:40 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from deap import creator
 from deap import tools
 import random
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # константы задачи
-ONE_MAX_LENGTH = 100  # длина подлежащей оптимизации битовой строки
+ONE_MAX_LENGTH = 100 # длина подлежащей оптимизации битовой строки
 # константы генетического алгоритма
-POPULATION_SIZE = 200  # количество индивидуумов в популяции
+POPULATION_SIZE = 100  # количество индивидуумов в популяции
 P_CROSSOVER = 0.9  # вероятность скрещивания
 P_MUTATION = 0.1  # вероятность мутации индивидуума
 MAX_GENERATIONS = 50  # максимальное количество поколений
 
-RANDOM_SEED = 42
+RANDOM_SEED = 41
 # на этапе экспериментирования требуется, чтобы результаты были воспроизводимы.
 # Для этого мы задаем какое-нибудь фиксированное начальное значение генератора случайных чисел
 random.seed(RANDOM_SEED)
@@ -34,31 +35,73 @@ toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individual
 
 
 def oneMaxFitness(individual):
-    return sum(individual), # Вернуть кортеж
+    return sum(individual),  # return a tuple # Вернуть кортеж
 
 
 toolbox.register("evaluate", oneMaxFitness)
 
 toolbox.register("select", tools.selTournament, tournsize=3)
 toolbox.register("mate", tools.cxOnePoint)
-toolbox.register("mutate", tools.mutFlipBit, indpd=1.0/ONE_MAX_LENGTH)
+toolbox.register("mutate", tools.mutFlipBit, indpb=1.0/ONE_MAX_LENGTH)
 
 
-if __name__ == '__main__':
+def main():
     population = toolbox.populationCreator(n=POPULATION_SIZE)
     generationCounter = 0
     fitnessValues = list(map(toolbox.evaluate, population))
 
     for individual, fitnessValue in zip(population, fitnessValues):
-        individual.fitness.value = fitnessValue
+        individual.fitness.values = fitnessValue
 
-    fitnessValues = [individual.fitness.value[0] for individual in population]
+    fitnessValues = [individual.fitness.values[0] for individual in population]
 
     maxFitnessValues = []
     meanFitnessValues = []
 
-    while generationCounter < MAX_GENERATIONS and max(fitnessValues) < ONE_MAX_LENGTH:
-        generationCounter += 1
+    while max(fitnessValues) < ONE_MAX_LENGTH and generationCounter < MAX_GENERATIONS:
+        generationCounter = generationCounter + 1
+        # apply the selection operator, to select the next generation's individuals:
         offspring = toolbox.select(population, len(population))
+        # clone the selected individuals:
         offspring = list(map(toolbox.clone, offspring))
-        pass
+
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            if random.random() < P_CROSSOVER:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
+
+        for mutant in offspring:
+            if random.random() < P_MUTATION:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+
+        freshIndividuals = [ind for ind in offspring if not ind.fitness.valid]
+        freshFitnessValues = list(map(toolbox.evaluate, freshIndividuals))
+        for individual, fitnessValue in zip(freshIndividuals, freshFitnessValues):
+            individual.fitness.values = fitnessValue
+
+        population[:] = offspring
+
+        fitnessValues = [individual.fitness.values[0] for individual in population]
+
+        maxFitness = max(fitnessValues)
+        meanFitness = sum(fitnessValues) / len(population)
+        maxFitnessValues.append(maxFitness)
+        meanFitnessValues.append(meanFitness)
+        print("- Поколение {}: Макс приспособ. = {}, Средняя приспособ. = {}".format(generationCounter, maxFitness, meanFitness))
+
+        best_index = fitnessValues.index(max(fitnessValues))
+        print("Лучший индивидуум = ", *population[best_index], "\n")
+
+    sns.set_style("whitegrid")
+    plt.plot(maxFitnessValues, color='red')
+    plt.plot(meanFitnessValues, color='green')
+    plt.xlabel('Поколение')
+    plt.ylabel('Макс/средняя приспособленность')
+    plt.title('Зависимость максимальной и средней приспособленности от поколения')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
